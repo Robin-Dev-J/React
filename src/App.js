1765006271f32d21ce0240cfbba4769adc23f476
Robin-Dev-J <robindev289@gmail.com>
2024-01-24 21:49:57 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import TestComp from './Components/FunctionComponents/TestComp';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TestComp/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
