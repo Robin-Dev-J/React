1dfb92b2d43aa3bec7540d6d6f423dc27c58f437
Robin-Dev-J <robindev289@gmail.com>
2024-01-25 09:27:31 +0530

    commit

--- src/Components/FunctionComponents/TestComp.jsx
+++ src/Components/FunctionComponents/TestComp.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TestComp(){
     return(
         <div>
-            <h1>This is a test Functional component Hola.</h1>
+            <h1>Camera Wowo.</h1>
         </div>
     );
 }
