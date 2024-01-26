84822f63aae23ac6be5a8b02eeadddae4e3f9148
Robin-Dev-J <robindev289@gmail.com>
2024-01-27 00:22:38 +0530

    react dev

--- src/Components/FunctionComponents/Login.jsx
+++ src/Components/FunctionComponents/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './LoginForm.css'; 
 import { FaUser, FaLock } from "react-icons/fa";
 
-const LoginForm = () => {
+const Login = () => {
   return (
     <div className='wrapper'>
       <form action=''>
@@ -28,4 +28,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default Login;
